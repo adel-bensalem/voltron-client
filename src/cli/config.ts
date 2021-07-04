c6534ac865feee2cb84d82cc1b42488d8e15af84
adel.bensalem1 <adel.bensalem@epitech.eu>
2021-07-04 17:33:33 +0200

    refactor cli layer

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,10 +1,9 @@
 import { program } from "commander";
 
 program
-  .option("-k, --key <key>", "SSH key file")
   .option("-h, --help", "Print manual")
-  .option("-e, --email <email>", "Account email", "")
-  .option("-p, --password <password>", "Account password", "");
+  .option("-e, --email [email]", "Account email", "")
+  .option("-p, --password [password]", "Account password", "");
 
 program.parse(process.argv);
 
